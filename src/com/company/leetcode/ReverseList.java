4da20cacb6152b3d2609cd3b3b56ba6938bf39d5
dauglas <tang85718@users.noreply.github.com>
2018-10-30 11:03:29 +0800

    反转链表 调整代码

--- src/com/company/leetcode/ReverseList.java
+++ src/com/company/leetcode/ReverseList.java
@@ -1,15 +1,17 @@
 package com.company.leetcode;
 
 
-
 public class ReverseList {
 
 
-     public static class ListNode {
-         int val;
-         ListNode next;
-         ListNode(int x) { val = x; }
-     }
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 
 
     public ListNode reverse(ListNode head, ListNode prev) {
@@ -28,36 +30,36 @@ public class ReverseList {
     }
 
     public static ListNode createList() {
-         int[] t1 = new int[] {1,2,3,4,5};
-         ListNode head = new ListNode(t1[0]);
+        int[] t1 = new int[]{1, 2, 3, 4, 5};
+        ListNode head = new ListNode(t1[0]);
 
-         ListNode cur = head;
-         for (int i=1; i<t1.length; i++) {
-             cur.next = new ListNode(t1[i]);
-             cur = cur.next;
-         }
+        ListNode cur = head;
+        for (int i = 1; i < t1.length; i++) {
+            cur.next = new ListNode(t1[i]);
+            cur = cur.next;
+        }
 
-         return head;
+        return head;
     }
 
     public static void showList(ListNode head) {
-         if (head == null) {
-             System.out.println();
-             return ;
-         }
+        if (head == null) {
+            System.out.println();
+            return;
+        }
 
-         System.out.print(head.val);
-         System.out.print(",");
+        System.out.print(head.val);
+        System.out.print(",");
 
-         showList(head.next);
+        showList(head.next);
     }
 
     public static void main(String[] args) {
-         ListNode head = createList();
+        ListNode head = createList();
         showList(head);
 
-         ReverseList reverseList = new ReverseList();
-         ListNode ret = reverseList.reverseList(head);
-         showList(ret);
+        ReverseList reverseList = new ReverseList();
+        ListNode ret = reverseList.reverseList(head);
+        showList(ret);
     }
 }
